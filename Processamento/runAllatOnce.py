b7789930850fce261d6799c1ac16c51fbd3cd8ac
JaoIndio <amralsantiago2@gmail.com>
2023-10-18 21:07:21 -0300

    Search Grid

--- Processamento/runAllatOnce.py
+++ Processamento/runAllatOnce.py
@@ -3,13 +3,59 @@
 
 import subprocess
 
-generalPahth=(f"Universal_pyEnv")
-SVRPahth=(f"{generalPahth}/SVR")
-linear_PCA=(f"{SVRPahth}/linear_PCA/linearPCA_env/src/SVR_linear.py")
-rbf_PCA=(f"{SVRPahth}/rbf_PCA/rbfPCA_env/src/SVR.py")
-rbf=(f"{SVRPahth}/rbf/rbf_env/src/SVR.py")
+generalPath=(f"Universal_pyEnv")
+
+SVRPath=(f"{generalPath}/SVR")
+linear_PCA=(f"{SVRPath}/linear_PCA/linearPCA_env/src/SVR_linear.py")
+rbf_PCA=(f"{SVRPath}/rbf_PCA/rbfPCA_env/src/SVR.py")
+rbf=(f"{SVRPath}/rbf/rbf_env/src/SVR.py")
+dt=(f"{generalPath}/DecisionTree/DecisionTree_env/src/DecisionTree.py")
+rf=(f"{generalPath}/RandomForest/rf_env/src/RandomForest.py")
+GradBoost=(f"{generalPath}/GradientBoost/GradBoost_env/src/GradBoost.py")
+
+#BrLib_MIR_path       =(f"{generalPath}/Brasil_SpectralLib_MIR")
+#SVRPath              =(f"{BrLib_MIR_path}/SVR")
+#linear_PCA_BrLib_MIR =(f"{SVRPath}/linear_PCA/linearPCA_env/src/SVR_linear.py")
+#rbf_PCA_BrLib_MIR    =(f"{SVRPath}/rbf_PCA/rbfPCA_env/src/SVR.py")
+#rbf_BrLib_MIR        =(f"{SVRPath}/rbf/rbf_env/src/SVR.py")
+#dt_BrLib_MIR         =(f"{BrLib_MIR_path}/DecisionTree/DecisionTree_env/src/DecisionTree.py")   
+#rf_BrLib_MIR         =(f"{BrLib_MIR_path}/RandomForest/rf_env/src/RandomForest.py")            
+#GradBoost_BrLib_MIR  =(f"{BrLib_MIR_path}/GradientBoost/GradBoost_env/src/GradBoost.py")      
+#PLSR_BrLib_MIR       =(f"{BrLib_MIR_path}/PLSR/PLSR_env/src/PLSR.py")                             
+
+#BrLib_NIR_path       =(f"{generalPath}/Brasil_SpectralLib_NIR")
+#SVRPath              =(f"{generalPath}/SVR")
+#linear_PCA_BrLib_NIR =(f"{SVRPath}/linear_PCA/linearPCA_env/src/SVR_linear.py")
+#rbf_PCA_BrLib_NIR    =(f"{SVRPath}/rbf_PCA/rbfPCA_env/src/SVR.py")
+#rbf_BrLib_NIR        =(f"{SVRPath}/rbf/rbf_env/src/SVR.py")
+#dt_BrLib_NIR         =(f"{BrLib_NIR_path}/DecisionTree/DecisionTree_env/src/DecisionTree.py"
+#rf_BrLib_NIR         =(f"{BrLib_NIR_path}/RandomForest/rf_env/src/RandomForest.py")         
+#GradBoost_BrLib_NIR  =(f"{BrLib_NIR_path}/GradientBoost/GradBoost_env/src/GradBoost.py")    
+#PLSR_BrLib_NIR       =(f"{BrLib_NIR_path}/PLSR/PLSR_env/src/PLSR.py")                       
+
+
 # List of scripts to run
-scripts_to_run = [linear_PCA, rbf_PCA, rbf]
+                   # DataSet com 40 amostras
+scripts_to_run = [linear_PCA, rbf_PCA, rbf, dt, rf, GradBoost \
+                 
+                # DataSet da Biblioteca Nacional
+                     #linear_PCA_BrLib_MI,\ 
+                     #rbf_PCA_BrLib_MIR,  \
+                     #rbf_BrLib_MIR,      \
+                     #dt_BrLib_MIR,       \
+                     #rf_BrLib_MIR,       \
+                     #GradBoost_BrLib_MIR,\
+                     #PLSR_BrLib_MIR,     \
+
+                # DataSet da Biblioteca Nacional
+                     #linear_PCA_BrLib_MI,\   
+                     #rbf_PCA_BrLib_MIR,  \
+                     #rbf_BrLib_MIR,      \
+                     #dt_BrLib_MIR,       \
+                     #rf_BrLib_MIR,       \
+                     #GradBoost_BrLib_MIR,\
+                     #PLSR_BrLib_MIR,     \
+                 ]
 
 # Run each script in a separate subprocess
 processes = []
